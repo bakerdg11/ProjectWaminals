32ab3b6fcb6cb566478ee0cb81707ecef3e6c657
Daniel Baker <daniel.baker@torontofilmschool.ca>
2025-03-29 12:11:17 -0400

    Initial Setup
    Created C++ script for the Game Mode and Player Controller.
    Created BasePlayer.cpp.
    
    Generated BP for each GM, PC, and BasePlayer.

---
+++ Source/Project_Waminals/Public/PlayerController/PWPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PWPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_WAMINALS_API APWPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
